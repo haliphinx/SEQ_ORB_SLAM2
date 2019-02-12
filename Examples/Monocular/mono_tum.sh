a856c60129d7de2dc89c1207f828509348c273b9
xhu <xhu0026@gmail.com>
2019-02-12 15:20:57 -0500

    add some file for TUM evaluation

---
+++ Examples/Monocular/mono_tum.sh
@@ -0,0 +1,4 @@
+./mono_tum \
+ /home/xhu/Code/ORB_SLAM2/Vocabulary/ORBvoc.txt \
+ /home/xhu/Code/ORB_SLAM2/Examples/Monocular/TUM2.yaml \
+ /home/xhu/Code/dataset/rgbd_dataset_freiburg2_xyz
---
+++ Examples/RGB-D/rgbd_tum.sh
@@ -0,0 +1,5 @@
+./rgbd_tum \
+ /home/xhu/Code/ORB_SLAM2/Vocabulary/ORBvoc.txt \
+ /home/xhu/Code/ORB_SLAM2/Examples/RGB-D/TUM2.yaml \
+ /home/xhu/Code/dataset/rgbd_dataset_freiburg2_xyz \
+ //home/xhu/Code/ORB_SLAM2/Examples/RGB-D/associations/fr2_xyz.txt
